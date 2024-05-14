f5ea6ff0c6df0ed50deca0d779fda7857521c9d1
Jorge <jd_escalona@hotmail.com>
2024-05-14 15:31:05 -0400

    integracion de Formulario

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
-  selector: 'app-dbz-list',
+  selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Input()
+  public CharacterList: Character[] = []
 
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,8 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html'
 })
 
-export class MainComponent{}
+export class MainComponent{
+
+
+  public character: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },
+  {
+    name: 'Goku',
+    power: 9500
+  },
+  {
+    name: 'Vegeta',
+    power: 7500
+  }
+];
+
+
+
+}
